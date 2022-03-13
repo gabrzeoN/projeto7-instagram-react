2d6a1e20752a32e51e85c6fe46acd978dba6a6b3
Gabriel Cari <g.cari@hotmail.com>
2022-03-13 12:07:52 -0300

    feat: Represetando em arrays de objetos: Stories, Posts.

--- src/components/Esquerda.js
+++ src/components/Esquerda.js
@@ -1,63 +1,99 @@
+const stories = [
+    {imagem: "assets/img/9gag.svg", usuario: "9gag"},
+    {imagem: "assets/img/meowed.svg", usuario: "meowed"},
+    {imagem: "assets/img/barked.svg", usuario: "barked"},
+    {imagem: "assets/img/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet"},
+    {imagem: "assets/img/wawawicomics.svg", usuario: "wawawicomics"},
+    {imagem: "assets/img/respondeai.svg", usuario: "respondeai"},
+    {imagem: "assets/img/filomoderna.svg", usuario: "filomoderna"},
+    {imagem: "assets/img/memeriagourmet.svg", usuario: "memeriagourmet"}
+];
+
+const posts = [
+    {
+        imagemUsuario: "assets/img/meowed.svg",
+        nomeUsuario: "meowed",
+        imagemConteudo: "assets/img/gato-telefone.svg",
+        imagemQuemCurtiu: "assets/img/respondeai.svg",
+        quemCurtiu: "respondeai",
+        quantidadeCurtidas: "101.523"
+    },
+    {
+        imagemUsuario: "assets/img/barked.svg",
+        nomeUsuario: "barked",
+        imagemConteudo: "assets/img/dog.svg",
+        imagemQuemCurtiu: "assets/img/adorable_animals.svg",
+        quemCurtiu: "adorable_animals",
+        quantidadeCurtidas: "99.159"
+    }
+];
+
 export default function Esquerda() {
     return (
         <div class="esquerda">
             <div class="stories">
-                <Story imagem="assets/img/9gag.svg" usuario="9gag" />
-                <Story imagem="assets/img/meowed.svg" usuario="meowed" />
-                <Story imagem="assets/img/barked.svg" usuario="barked" />
-                <Story imagem="assets/img/nathanwpylestrangeplanet.svg" usuario="nathanwpylestrangeplanet" />
-                <Story imagem="assets/img/wawawicomics.svg" usuario="wawawicomics" />
-                <Story imagem="assets/img/respondeai.svg" usuario="respondeai" />
-                <Story imagem="assets/img/filomoderna.svg" usuario="filomoderna" />
-                <Story imagem="assets/img/memeriagourmet.svg" usuario="memeriagourmet" />
-
+                {
+                    stories.map((story) => {
+                        const {imagem, usuario} = story;
+                        return(
+                            <Story imagem={imagem} usuario={usuario} />
+                        );
+                    })
+                }
                 <div class="setinha">
                     <ion-icon name="chevron-forward-circle"></ion-icon>
                 </div>
             </div>
-            
-            <div class="posts">
-                <Post
-                    imagemUsuario="assets/img/meowed.svg"
-                    nomeUsuario="meowed"
-                    imagemConteudo="assets/img/gato-telefone.svg"
-                    imagemQuemCurtiu="assets/img/respondeai.svg"
-                    quemCurtiu="respondeai"
-                    quantidadeCurtidas="101.523"
-                />
-                <Post
-                    imagemUsuario="assets/img/barked.svg"
-                    nomeUsuario="barked"
-                    imagemConteudo="assets/img/dog.svg"
-                    imagemQuemCurtiu="assets/img/adorable_animals.svg"
-                    quemCurtiu="adorable_animals"
-                    quantidadeCurtidas="99.159"
-                />
+
+            <div class="posts">      
+                {
+                    posts.map((post) => {
+                        const {
+                            imagemUsuario, nomeUsuario, imagemConteudo,
+                            imagemQuemCurtiu, quemCurtiu, quantidadeCurtidas
+                        } = post;
+                        return(
+                            <Post
+                                imagemUsuario={imagemUsuario}
+                                nomeUsuario={nomeUsuario}
+                                imagemConteudo={imagemConteudo}
+                                imagemQuemCurtiu={imagemQuemCurtiu}
+                                quemCurtiu={quemCurtiu}
+                                quantidadeCurtidas={quantidadeCurtidas}
+                            />
+                        );
+                    })
+                }   
             </div>
         </div>
     );
 }
 
 function Story(props) {
+    const {imagem, usuario} = props;
     return (
         <div class="story">
             <div class="imagem">
-                <img src={props.imagem} />
+                <img src={imagem} />
             </div>
             <div class="usuario">
-                {props.usuario}
+                {usuario}
             </div>
         </div>
     );
 }
 
 function Post(props) {
+    const {
+        imagemUsuario, nomeUsuario, imagemConteudo,
+        imagemQuemCurtiu, quemCurtiu, quantidadeCurtidas
+    } = props;
     return (
         <div class="post">
             <div class="topo">
                 <div class="usuario">
-                    <img src={props.imagemUsuario} />
-                    {props.nomeUsuario}
+                    <img src={imagemUsuario} />
+                    {nomeUsuario}
                 </div>
                 <div class="acoes">
                     <ion-icon name="ellipsis-horizontal"></ion-icon>
@@ -65,7 +101,7 @@ function Post(props) {
             </div>
 
             <div class="conteudo">
-                <img src={props.imagemConteudo} />
+                <img src={imagemConteudo} />
             </div>
 
             <div class="fundo">
@@ -81,9 +117,9 @@ function Post(props) {
                 </div>
 
                 <div class="curtidas">
-                    <img src={props.imagemQuemCurtiu} />
+                    <img src={imagemQuemCurtiu} />
                     <div class="texto">
-                        Curtido por <strong>{props.quemCurtiu}</strong> e <strong>outras {props.quantidadeCurtidas} pessoas</strong>
+                        Curtido por <strong>{quemCurtiu}</strong> e <strong>outras {quantidadeCurtidas} pessoas</strong>
                     </div>
                 </div>
             </div>
